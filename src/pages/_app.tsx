7bca2a8987ae129071a64e681cc3b4f2218c100a
Andriannus Parasian <andriannus.parasian@efishery.com>
2022-07-20 10:25:56 +0700

    chore: install and setup font-awesome

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import type { AppProps } from "next/app";
+import { config, library } from "@fortawesome/fontawesome-svg-core";
+import { faArrowLeft, faSearch } from "@fortawesome/free-solid-svg-icons";
 
+import "@fortawesome/fontawesome-svg-core/styles.css";
 import "@/styles/globals.scss";
+
+config.autoAddCss = false;
+library.add(faArrowLeft, faSearch);
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return <Component {...pageProps} />;
