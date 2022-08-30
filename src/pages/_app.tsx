83269bf356f7e6801aa65d12b951037c84d7bc76
Andriannus Parasian <andriannus.parasian@efishery.com>
2022-08-30 22:29:21 +0700

    refactor: set layout component globally

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,29 @@
 import type { AppProps } from "next/app";
+import dynamic from "next/dynamic";
 import { config, library } from "@fortawesome/fontawesome-svg-core";
-import { faArrowLeft, faSearch } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArchive,
+  faArrowLeft,
+  faSearch,
+} from "@fortawesome/free-solid-svg-icons";
 
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import "@/styles/globals.scss";
 
+const DefaultLayout = dynamic(
+  () => import("@/layouts/default/default.component"),
+  { ssr: false },
+);
+
 config.autoAddCss = false;
-library.add(faArrowLeft, faSearch);
+library.add(faArchive, faArrowLeft, faSearch);
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <DefaultLayout>
+      <Component {...pageProps} />
+    </DefaultLayout>
+  );
 }
 
 export default MyApp;
